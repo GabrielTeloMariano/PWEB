20c354dc5df4d02026889ec01d54682b92cff162
Gabriel Telo Mariano <116767607+GabrielTeloMariano@users.noreply.github.com>
2024-09-18 21:22:49 -0300

    Add files via upload

--- Atividade6/atividade6.js
+++ Atividade6/atividade6.js
@@ -1,22 +1,22 @@
 function media (){
     var nome = prompt("Digite seu nome:");
-    var nota1 = prompt("Digite a nota 1: ");
-    var nota2 = prompt("Digite a nota 2: ");
-    var nota3 = prompt("Digite a nota 3: ");
-    var nota4 =prompt("Digite a nota 4: ");
+    var nota1 = parseFloat(prompt("Digite a nota 1: "));
+    var nota2 = parseFloat(prompt("Digite a nota 2: "));
+    var nota3 = parseFloat(prompt("Digite a nota 3: "));
+    var nota4 = parseFloat(prompt("Digite a nota 4: "));
 
-    var media = (parseFloat(nota1) + parseFloat(nota2) + parseFloat(nota3) + parseFloat(nota4)) / 4;
+    var media = (nota1 + nota2 + nota3 + nota4) / 4;
     alert("Sua média, " + nome + ", é: " + media);
 }
 
 function operacoes(){
-    var num1 = prompt("Digite o primeiro número: ");
-    var num2 = prompt("Digite o segundo número: ");
-    var soma = parseFloat(num1) + parseFloat(num2);
-    var subtracao = parseFloat(num1) - parseFloat(num2);
-    var multiplicacao = parseFloat(num1) * parseFloat(num2);
-    var divisao = parseFloat(num1) / parseFloat(num2);
-    var resto = parseFloat(num1) % parseFloat(num2);
+    var num1 = parseFloat(prompt("Digite o primeiro número: "));
+    var num2 = parseFloat(prompt("Digite o segundo número: "));
+    var soma = num1 + num2;
+    var subtracao = num1 - num2;
+    var multiplicacao = num1 * num2;
+    var divisao = num1 / num2;
+    var resto = num1 % num2;
     alert("A soma é: " + soma.toFixed(2));
     alert("A subtração é: " + subtracao.toFixed(2));
     alert("A multiplicação é: " + multiplicacao.toFixed(2));
